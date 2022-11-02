6ec42d86d4611017a100e05bb1b6a452d3962f1a
Krzysztof Zajączkowski <Krzysztof.m.zajaczkowski@gmail.com>
2022-11-02 19:30:57 +0100

    #1 Add solution with validation library
    * #1 Add solution with FunctionalValidation shared library
    * #1 Add FunctionalValidation tests workflow
    * #1 Add workflow for publishing NuGet package
    * #1 Load shared variables in defined workflows

---
+++ src/shared/FunctionalValidation/IValidator.cs
@@ -0,0 +1,18 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace FunctionalValidation
+{
+    /// <summary>
+    /// Interface that defines validator for <typeparamref name="TType"/> type.
+    /// </summary>
+    /// <typeparam name="TType">Type that is validated by implementation of this interface.</typeparam>
+    public interface IValidator<TType>
+    {
+        /// <summary>
+        /// Validates <paramref name="value"/> and returns <see cref="Result{TType}"/> that contains <paramref name="value"/> if validation is successful or error message if validation fails.
+        /// </summary>
+        /// <param name="value">Value that is validated.</param>
+        /// <returns>Result of validation.</returns>
+        Result<TType> Validate(TType value);
+    }
+}
