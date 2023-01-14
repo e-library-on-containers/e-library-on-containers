817ee27d3f9a473b2b21af4d3f8dd8a68852c68b
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2023-01-14 22:13:16 +0100

    #15 Update Infrastructure layer

---
+++ src/identity/infrastructure/eLibraryOnContainers.Identity.Infrastructure/Models/ReadRole.cs
@@ -0,0 +1,13 @@
+﻿namespace eLibraryOnContainers.Identity.Infrastructure.Models;
+
+public class ReadRole
+{
+    public Guid UserId { get; }
+    public string RoleName { get; }
+
+    public ReadRole(Guid userId, string roleName)
+    {
+        UserId = userId;
+        RoleName = roleName;
+    }
+}
--- src/identity/infrastructure/eLibraryOnContainers.Identity.Infrastructure/Repositories/UsersRepository.cs
+++ src/identity/infrastructure/eLibraryOnContainers.Identity.Infrastructure/Repositories/UsersRepository.cs
@@ -5,6 +5,7 @@ using eLibraryOnContainers.Identity.Application.Repositories;
 using eLibraryOnContainers.Identity.Domain.Entities;
 using eLibraryOnContainers.Identity.Domain.ValueObjects;
 using eLibraryOnContainers.Identity.Infrastructure.Common;
+using eLibraryOnContainers.Identity.Infrastructure.Models;
 using FunctionalValidation.Errors;
 
 namespace eLibraryOnContainers.Identity.Infrastructure.Repositories;
@@ -37,10 +38,33 @@ public class UsersRepository : IUsersRepository
         return CatchDbException(async x =>
         {
             await using var connection = await _connectionFactory.CreateAsync();
-            var user = await connection.QuerySingleOrDefaultAsync<User>("SELECT * FROM Users WHERE Email = @Email;",
-                new { Email = x.Address });
-            return Result.SuccessIf<User, ApplicationError>(user != null, user!,
-                new NotFoundError("User not found."));
+            var result = await connection.QueryAsync<ReadUser, ReadRole, ReadUser>(@"
+                SELECT * FROM users u
+                LEFT JOIN user_roles ur
+                ON u.id = ur.user_id
+                WHERE email = @Email;",
+                (readUser, readRole) =>
+                {
+                    readUser.Roles.Add(readRole);
+                    return readUser;
+                },
+                new { Email = x.Address },
+                splitOn: "user_id");
+
+            var user = result.FirstOrDefault();
+
+            return Result
+                .SuccessIf<ReadUser, ApplicationError>(user != null, user!, new NotFoundError("User not found."))
+                .Bind(u => Email.From(u.Email)
+                    .Map(mail => (u, mail)))
+                .Bind(t => HashedPassword.FromHashedPassword(t.u.Password)
+                    .Map(password => (t.u, t.mail, password)))
+                .Bind(t => FullName.From(t.u.FirstName, t.u.LastName)
+                    .Map(fullName => (t.u, t.mail, t.password, fullName)))
+                .Bind(t => t.u.Roles.Select(x => RoleName.From(x.RoleName))
+                    .Combine(errors => errors.First())
+                    .Map(roleNames => (t.u, t.mail, t.password, t.fullName, roleNames)))
+                .Bind(t => User.From(t.u.Id, t.mail, t.fullName, t.password, t.roleNames.ToArray()));
         }, email);
     }
 
@@ -50,9 +74,9 @@ public class UsersRepository : IUsersRepository
         {
             await using var connection = await _connectionFactory.CreateAsync();
             var affectedRows = await connection.ExecuteAsync(@"
-                UPDATE Users
-                SET Password = @Password
-                WHERE Id = @Id;",
+                UPDATE users
+                SET password = @Password
+                WHERE id = @Id;",
                 new
                 {
                     Password = x.Password.Value,
@@ -69,19 +93,34 @@ public class UsersRepository : IUsersRepository
         return CatchDbException(async x =>
         {
             await using var connection = await _connectionFactory.CreateAsync();
-            var affectedRows = await connection.ExecuteAsync(@"
-                INSERT INTO Users VALUES (@Id, @Email, @Password, @FirstName, @LastName);",
-                new
-                {
-                    x.Id,
-                    Email = x.Email.Address,
-                    Password = x.Password.Value,
-                    x.FullName.FirstName,
-                    x.FullName.LastName
-                });
+            await using (var transaction = await connection.BeginTransactionAsync())
+            {
+                var usersAffectedRows = await connection.ExecuteAsync(@"
+                INSERT INTO users VALUES (@Id, @Email, @Password, @FirstName, @LastName);",
+                    new
+                    {
+                        x.Id,
+                        Email = x.Email.Address,
+                        Password = x.Password.Value,
+                        x.FullName.FirstName,
+                        x.FullName.LastName
+                    },
+                    transaction);
 
-            return Result.SuccessIf<bool, ApplicationError>(affectedRows == 1, true,
-                new NotFoundError("User not found."));
+                var rolesAffectedRows = await connection.ExecuteAsync(@"
+                INSERT INTO user_roles VALUES (@UserId, @RoleName);",
+                    x.Roles.Select(rn => new
+                    {
+                        UserId = x.Id,
+                        RoleName = rn.Value
+                    }),
+                    transaction);
+
+                return await Result.SuccessIf<bool, ApplicationError>(
+                        usersAffectedRows == 1 && rolesAffectedRows == x.Roles.Count,
+                        true, new UnknownError("Could not register user."))
+                    .Tap(() => transaction.CommitAsync());
+            }
         }, user);
     }
 }
