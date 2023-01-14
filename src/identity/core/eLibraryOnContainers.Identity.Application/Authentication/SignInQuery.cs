5074a957ca3f9f94c6b6701e954d6804b150a13b
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2023-01-14 21:14:00 +0100

    #15 Implement Application layer

---
+++ src/identity/core/eLibraryOnContainers.Identity.Application/Authentication/SignInQuery.cs
@@ -0,0 +1,44 @@
+﻿using CSharpFunctionalExtensions;
+using eLibraryOnContainers.Identity.Application.Dtos;
+using eLibraryOnContainers.Identity.Application.Repositories;
+using eLibraryOnContainers.Identity.Application.Services;
+using eLibraryOnContainers.Identity.Domain.Errors;
+using eLibraryOnContainers.Identity.Domain.ValueObjects;
+using FunctionalValidation.Errors;
+using MediatR;
+
+namespace eLibraryOnContainers.Identity.Application.Authentication;
+
+public class SignInQuery : IRequest<Result<TokenDto, ApplicationError>>
+{
+    public string Email { get; }
+    public string Password { get; }
+
+    public SignInQuery(string email, string password)
+    {
+        Email = email;
+        Password = password;
+    }
+}
+
+public class SignInQueryHandler : IRequestHandler<SignInQuery, Result<TokenDto, ApplicationError>>
+{
+    private readonly IUsersRepository _usersRepository;
+    private readonly IAuthService _authService;
+
+    public SignInQueryHandler(IUsersRepository usersRepository, IAuthService authService)
+    {
+        _usersRepository = usersRepository;
+        _authService = authService;
+    }
+
+    public async Task<Result<TokenDto, ApplicationError>> Handle(SignInQuery request, CancellationToken cancellationToken) =>
+        await Email.From(request.Email)
+            .Bind(email => _usersRepository.GetUserByEmailAsync(email)
+                .Map(user => (user, email)))
+            .Bind(t => HashedPassword.FromRawPassword(request.Password, HashedPassword.DefaultHasher)
+                .Map(password => (t.user, password)))
+            .Ensure(t => t.user.Password.Equals(t.password), new PasswordsDoesNotMatch())
+            .Map(t => t.user)
+            .Bind(user => _authService.SingIn(user));
+}
---
+++ src/identity/core/eLibraryOnContainers.Identity.Application/Services/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using CSharpFunctionalExtensions;
+using eLibraryOnContainers.Identity.Application.Dtos;
+using eLibraryOnContainers.Identity.Domain.Entities;
+using FunctionalValidation.Errors;
+
+namespace eLibraryOnContainers.Identity.Application.Services;
+
+public interface IAuthService
+{
+    Result<TokenDto, ApplicationError> SingIn(User user);
+}
