4b30c70a2e6e1f62f33c1c4ff355e8612a60b230
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2023-01-14 21:13:07 +0100

    #15 Implement Domain layer

---
+++ src/identity/core/eLibraryOnContainers.Identity.Domain/Errors/NoUserRoles.cs
@@ -0,0 +1,10 @@
+﻿using FunctionalValidation.Errors;
+
+namespace eLibraryOnContainers.Identity.Domain.Errors;
+
+public class NoUserRoles : ValidationError
+{
+    public NoUserRoles() : base("Can not create user without any roles.")
+    {
+    }
+}
---
+++ src/identity/tests/eLibraryOnContainers.Identity.Tests/New.cs
@@ -0,0 +1,12 @@
+﻿using eLibraryOnContainers.Identity.Domain.Entities;
+using eLibraryOnContainers.Identity.Domain.ValueObjects;
+
+namespace eLibraryOnContainers.Identity.Tests;
+
+public static class New
+{
+    public static HashedPassword HashedPassword(string rawPassword) =>
+        Domain.ValueObjects.HashedPassword.FromRawPassword(rawPassword, Functions.HashPassword).Value;
+
+    public static RoleName RoleName(string value) => Domain.ValueObjects.RoleName.From(value).Value;
+}
