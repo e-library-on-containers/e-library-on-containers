bd214e4f253a73105a0608c00604ad03d44648bd
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2023-01-16 00:19:57 +0100

    #15 API layer cleanup

---
+++ src/identity/api/eLibraryOnContainers.Identity.Api/Infrastructure/OptionsConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using eLibraryOnContainers.Identity.Infrastructure.Options;
+using Microsoft.Extensions.Options;
+
+namespace eLibraryOnContainers.Identity.Api.Infrastructure;
+
+public static class OptionsConfiguration
+{
+    public static IServiceCollection AddOptions(this IServiceCollection services, IConfiguration configuration) =>
+        services.Configure<AuthOptions>(configuration.GetAuthSection())
+            .AddSingleton<IOptions<IAuthOptions>>(provider => provider.GetRequiredService<IOptions<AuthOptions>>())
+            .Configure<SqlOptions>(options =>
+                options.ConnectionString = configuration.GetConnectionString("DefaultConnection"))
+            .AddSingleton<IOptions<ISqlOptions>>(provider => provider.GetRequiredService<IOptions<SqlOptions>>());
+
+    private static IConfiguration GetAuthSection(this IConfiguration configuration) =>
+        configuration.GetSection("AuthOptions");
+
+    public static AuthOptions GetAuthOptions(this IConfiguration configuration) =>
+        configuration.GetAuthSection().Get<AuthOptions>();
+}
---
+++ src/identity/api/eLibraryOnContainers.Identity.Api/Infrastructure/PipelineConfiguration.cs
@@ -0,0 +1,21 @@
+﻿namespace eLibraryOnContainers.Identity.Api.Infrastructure;
+
+public static class PipelineConfiguration
+{
+    public static WebApplication UsePipeline(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapControllers();
+        return app;
+    }
+}
