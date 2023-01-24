e9bf26eefa62b3ac04125b5c628882a354c68ef7
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2023-01-24 23:04:36 +0100

    #32 Publish Identity images to Docker Hub

--- dev-compose.ps1
+++ dev-compose.ps1
@@ -1,1 +1,1 @@
-& docker-compose -f docker-compose.dev.yml --env-file .dev-env @args
+& docker-compose -f docker-compose.yml -f docker-compose.dev.yml --env-file .dev-env @args
--- dev-compose.sh
+++ dev-compose.sh
@@ -1,1 +1,1 @@
-docker-compose -f docker-compose.dev.yml --env-file .dev-env "$@"
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml --env-file .dev-env "$@"
