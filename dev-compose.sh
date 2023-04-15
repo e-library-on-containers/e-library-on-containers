6c9aab2c76cf42860e5225ba5152b9b9eff26813
Krzysztof Zajączkowski <Krzysztof.m.zajaczkowski@gmail.com>
2023-04-15 19:34:10 +0200

    #101 Update docker-compose files (PR: #102)

--- dev-compose.sh
+++ dev-compose.sh
@@ -1,1 +1,18 @@
-docker-compose -f docker-compose.yml -f docker-compose.dev.yml --env-file .dev-env "$@"
+#!/bin/bash
+
+LOCAL_BUILD=false
+
+while getopts "l" opt; do
+  case ${opt} in
+    l) LOCAL_BUILD=true ;;
+    *) echo "Invalid option: -$OPTARG" >&2 ;;
+  esac
+done
+
+if [ "$LOCAL_BUILD" = true ]
+  then
+    shift 
+    docker-compose -f docker-compose.yml -f docker-compose.dev.yml -f docker-compose.local.yml --env-file .dev-env "$@"
+  else
+    docker-compose -f docker-compose.yml -f docker-compose.dev.yml --env-file .dev-env "$@"
+fi
