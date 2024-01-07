035aedf60d12ce053e0f9214deeff441542ff353
Krzysztof Zajączkowski <Krzysztof.m.zajaczkowski@gmail.com>
2024-01-07 23:49:31 +0100

    #135 Fix movies (PR: #136)

---
+++ src/movies/Movies.Database/Scripts/Script0003 - People cascade foreign keys.sql
@@ -0,0 +1,21 @@
+﻿ALTER TABLE Actors
+    DROP CONSTRAINT actors_personid_fkey;
+
+ALTER TABLE Actors
+    ADD CONSTRAINT actors_personid_fkey
+    FOREIGN KEY (PersonId) REFERENCES People(Id) ON DELETE CASCADE;
+
+ALTER TABLE Screenwriters
+    DROP CONSTRAINT screenwriters_personid_fkey;
+
+ALTER TABLE Screenwriters
+    ADD CONSTRAINT screenwriters_personid_fkey
+    FOREIGN KEY (PersonId) REFERENCES People(Id) ON DELETE CASCADE;
+
+ALTER TABLE Directors
+    DROP CONSTRAINT directors_personid_fkey;
+
+ALTER TABLE Directors
+    ADD CONSTRAINT directors_personid_fkey
+    FOREIGN KEY (PersonId) REFERENCES People(Id) ON DELETE CASCADE;
+
