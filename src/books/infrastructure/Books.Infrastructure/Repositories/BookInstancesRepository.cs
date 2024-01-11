60c7046531f49549963fd1d94727e4785cdedb75
Krzysztof Zajączkowski <Krzysztof.m.zajaczkowski@gmail.com>
2024-01-11 23:29:21 +0100

    #141 Fix book instances get all (PR: #142)

--- src/books/infrastructure/Books.Infrastructure/Repositories/BookInstancesRepository.cs
+++ src/books/infrastructure/Books.Infrastructure/Repositories/BookInstancesRepository.cs
@@ -55,15 +55,10 @@ namespace Books.Infrastructure.Repositories
             }
         }
 
-        public async Task<List<BookInstance>> GetAll(bool includePreview = false)
+        public async Task<List<BookInstance>> GetAll()
         {
             var sql = "SELECT * FROM BookInstances b";
 
-            if (!includePreview)
-            {
-                sql += " WHERE b.InPreview = 'FALSE'";
-            }
-            
             using (var connection = new NpgsqlConnection(configuration.GetConnectionString("DapperConnection")))
             {
                 connection.Open();
